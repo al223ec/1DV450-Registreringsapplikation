edb391fd31c21c3ea8a7fa7543b6cee991ee29e2
al223ec <al223ec@student.lnu.se>
2015-01-29 14:01:12 +0100

    User model och sqlite test och valideringsregler

--- register_app/test/integration/site_layout_test.rb
+++ register_app/test/integration/site_layout_test.rb
@@ -9,7 +9,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", register_path
   end
 end
 
