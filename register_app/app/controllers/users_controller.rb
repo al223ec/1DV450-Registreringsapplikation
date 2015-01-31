b6b792d962bd6e24bbc55611adbad341e83accf1
al223ec <al223ec@student.lnu.se>
2015-01-31 21:49:59 +0100

    Form

--- register_app/app/controllers/users_controller.rb
+++ register_app/app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    #debugger ctrl+D för att fortsätta; We can treat this like a Rails console
+  end
+
   def new
+	@user = User.new
   end
 end
