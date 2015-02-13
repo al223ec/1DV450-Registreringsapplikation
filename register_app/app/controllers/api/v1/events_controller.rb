7cae288da3669af097097784fe07807442b588ab
al223ec <al223ec@student.lnu.se>
2015-02-13 14:53:15 +0100

    Call

--- register_app/app/controllers/api/v1/events_controller.rb
+++ register_app/app/controllers/api/v1/events_controller.rb
@@ -9,10 +9,23 @@ class Api::V1::EventsController < ActionController::Base
 	
 	def show
 		application = Application.find_by(key: params[:id])
+		
+		if application.nil? 
+			respond_with_errormessage("Felaktig nyckel din tomte!")
+			return
+		end
+
+		call = application.calls.build
+		call.ip = request.remote_ip
+		call.caller = request.env['HTTP_USER_AGENT'] 
+		# call.save # spara request
+
 		respond_with application
 	end
 
 	private 
+	# http://futureshock-ed.com/2011/03/04/http-status-code-symbols-for-rails/
 		def respond_with_errormessage(message)
+			render :json => { :error => 1, :message => message }, :status => :bad_request
 		end
 end
